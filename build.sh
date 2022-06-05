e64b23acefb6c95a8a77901f19f99b329c4aeb57
Haxk20 <16738302+Haxk20@users.noreply.github.com>
2022-06-05 09:25:18 +0200

    build.sh: Add support for building single board at a time

--- build.sh
+++ build.sh
@@ -4,18 +4,42 @@ boards=("FLY-FLYF407ZG" "Octopus-STM32F429" "SKR1.34-Turbo" "SKR1.34-Non-Turbo" 
 
 git clone https://github.com/Klipper3d/klipper.git
 
-for board in "${boards[@]}"
-do
-	cd klipper
-	echo "Copying new config"
-	cp ../$board .config
-	echo "Compiling klipper for $board"
-	make KCONFIG_CONFIG=../$board
-	echo "Cleaning up the mess we did"
-	# Distclean removes also the .config files for us
-	make distclean
-	cd ..
-	# This is an directory mounted from the host system
-	echo "Copying built image to output directory"
-	cp klipper/out/klipper.bin /out/$board-firmware.bin
-done
+if [ ! -z "$BOARD" ]
+then
+	for board in "${boards[@]}"
+	do
+		cd klipper
+		echo "Copying new config"
+		cp ../$board .config
+		echo "Compiling klipper for $board"
+		make KCONFIG_CONFIG=../$board
+		echo "Cleaning up the mess we did"
+		# Distclean removes also the .config files for us
+		make distclean
+		cd ..
+		# This is an directory mounted from the host system
+		echo "Copying built image to output directory"
+		cp klipper/out/klipper.bin /out/$board-firmware.bin
+	done
+else
+	if [[ " ${boards[*]} " == *" $BOARD "* ]]; then
+		cd klipper
+		echo "Copying new config"
+		cp ../$BOARD .config
+		echo "Compiling klipper for $BOARD"
+		make KCONFIG_CONFIG=../$BOARD
+		echo "Cleaning up the mess we did"
+		# Distclean removes also the .config files for us
+		make distclean
+		cd ..
+		# This is an directory mounted from the host system
+		echo "Copying built image to output directory"
+		cp klipper/out/klipper.bin /out/$BOARD-firmware.bin
+	else
+		echo "$BOARD is not a recognized board. Please check that the enviroment variable BOARD is one of the following:"
+		for board in "${boards[@]}"
+		do
+			echo "$board"
+		done
+	fi
+fi
